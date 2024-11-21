9bf37c8f7c748c12a6085d2fffdba9f797e6ab25
Svafnir42 <hivay.zol@yandex.ru>
2024-11-21 20:25:32 +0400

    add project

---
+++ ComProject/src/main/java/com/example/ComProject/ComProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.ComProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComProjectApplication.class, args);
+	}
+
+}
